fe8c0477181f91951fdc84449c1e8d02dc618f97
jax <jjackson17mv@gmail.com>
2019-07-15 18:01:50 -0700

    still need to fix index.js onClick

--- clickygame/src/components/Card/index.js
+++ clickygame/src/components/Card/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import "../../styles/card.css"
 
 const Card = props => (
-  <div className="card" onClick={() => props.cardClick(props.id)}>
+  <div className="card" onClick={() => props.cardCount(props.id)}>
+  
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
